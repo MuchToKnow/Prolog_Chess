f2c7fe06d89a1364561b48e50ddfc9049be0c47e
annie <qingyuew@alumni.ubc.ca>
2019-12-08 16:31:35 -0800

    add turn to print, add exit function

--- input.pl
+++ input.pl
@@ -1,11 +1,19 @@
 getMove(OldX, OldY, NewX, NewY):-
 	write("Your move: "),
 	read(X),
+	(
+	  	X = 'exit',
+	  	halt
+	;
 	name(X,[FromX,FromY,ToX,ToY]),
 	convertLetter(FromX, OldX),
 	convertLetter(ToX, NewX),
 	convertNumber(FromY, OldY),
-	convertNumber(ToY, NewY).
+	convertNumber(ToY, NewY),!
+	;
+	  	write('Wrong format ( enter like <a1b2.> '),nl,
+	  	fail
+	).
 
 
 convertLetter(FromL, To):-
--- output.pl
+++ output.pl
@@ -3,11 +3,29 @@
 % Empty square:
 % piece(X, Y, e, na)
 
-printBoard(Turn, Board1):-
+% printBoard(Turn, Board1):-
+%   empty_board(Board),
+%   (
+%   Turn =:= 'w' ->
+%     replaceBoard(Board, Board1, Board2);
+%   Turn =:= 'b' ->
+%     replaceBoardDiagonal(Board, Board1, Board2)
+%     ),
+%   print(Board2).
+
+printBoard('white', Board1):-
   empty_board(Board),
-  replaceBoard(Board, Board1, Board2),
+  replaceBoardDiagonal(Board, Board1, Board2),
+  write('Enter moves like <a2d4.>. Enter <exit.> to quit'),nl,
+	write('It is WHITE turn: '),nl,
   print(Board2).
 
+printBoard('black', Board1):-
+  empty_board(Board),
+  replaceBoard(Board, Board1, Board2),
+  write('Enter moves like <a2d4.>. Enter <exit.> to quit'),nl,
+	write('It is BLACK turn: '),nl,
+  print(Board2).
 
 % actually print the board by each rows
 print([R1,R2,R3,R4,R5,R6,R7,R8]):-
@@ -32,12 +50,23 @@ replaceBoard(B1, [H], B2):- replacePiece(B1, H, B2).
 replaceBoard(B1, [H|Pieces], B3):-
   replacePiece(B2,H,B3),replaceBoard(B1,Pieces,B2).
 
+
+replaceBoardDiagonal(B1, [H], B2):- replacePieceDiagonal(B1, H, B2).
+replaceBoardDiagonal(B1, [H|Pieces], B3):-
+  replacePieceDiagonal(B2,H,B3),replaceBoardDiagonal(B1,Pieces,B2).
 % replacePiece(Board, Piece, Board2)
 % x-position => (8-x)th row, y-position => (y-1)th element in the list, replace the emptycell to A type
 % x-position starts from 1th row
 replacePiece(_,piece(_, _, 'e', 'na'),_).
-replacePiece(B1,piece(X, Y, T, C),B2):-
-  X1 is 8-X, Y1 is Y-1,
+replacePiece(B1,piece(Y, X, T, C),B2):-
+  X1 is X-1, Y1 is Y-1,
+  X1 >= 0, Y1 >= 0, X1 < 9, Y1 < 9,
+  chessType(T,C,Z),
+  replace(B1,X1, Y1, Z, B2).
+
+
+replacePieceDiagonal(B1,piece(Y, X, T, C),B2):-
+  X1 is 8-X, Y1 is 8-Y,
   X1 >= 0, Y1 >= 0, X1 < 9, Y1 < 9,
   chessType(T,C,Z),
   replace(B1,X1, Y1, Z, B2).
@@ -72,7 +101,6 @@ chessType('b','b','\u2657').
 chessType('q','b','\u2655').
 chessType('k','b','\u2654').
 
-empty_string([[' ',' ',' ',' ',' ',' ',' ', ' '], [' ',' ',' ',' ',' ',' ',' ', ' '], [' ',' ',' ',' ',' ',' ',' ', ' '], [' ',' ',' ',' ',' ',' ',' ', ' '], [' ',' ',' ',' ',' ',' ',' ', ' '], [' ',' ',' ',' ',' ',' ',' ', ' '], [' ',' ',' ',' ',' ',' ',' ', ' '], [' ',' ',' ',' ',' ',' ',' ', ' ']]).
 empty_board([['\u2665','\u2665','\u2665','\u2665','\u2665','\u2665','\u2665','\u2665'],
 								['\u2665','\u2665','\u2665','\u2665','\u2665','\u2665','\u2665','\u2665'],
 								['\u2665','\u2665','\u2665','\u2665','\u2665','\u2665','\u2665','\u2665'],
